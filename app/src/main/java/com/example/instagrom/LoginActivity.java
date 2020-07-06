2a41102a9ab585cd90a242e18747cbb3ed6d83dc
Thu's <thuvo@fb.com>
2020-07-06 18:05:04 -0400

    login page and main activity layout

--- app/src/main/java/com/example/instagrom/LoginActivity.java
+++ app/src/main/java/com/example/instagrom/LoginActivity.java
@@ -2,13 +2,62 @@ package com.example.instagrom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.instagrom.databinding.ActivityLoginBinding;
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    public static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        final ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        if(ParseUser.getCurrentUser() != null){
+            goMainActivity();
+        }
+
+        binding.loginButtom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick login button");
+                String username = binding.usernameEditText.getText().toString();
+                String password = binding.passwordEditText.getText().toString();
+                loginUser(username, password);
+            }
+        });
+    }
+
+    private void loginUser(String name, String pass){
+        Log.i(TAG, "Attempting to login user: " + name);
+
+        ParseUser.logInInBackground(name, pass, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Issue with login", e);
+                    return;
+                }
+                goMainActivity();
+                Toast.makeText(LoginActivity.this,"Login Success!",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
--- app/src/main/java/com/example/instagrom/MainActivity.java
+++ app/src/main/java/com/example/instagrom/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.instagrom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.instagrom.databinding.ActivityLoginBinding;
+import com.example.instagrom.databinding.ActivityMainBinding;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        
+        queryPosts();
+    }
+
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Issue with getting posts" );
+                    return;
+                }
+
+                for (Post post : posts){
+                    Log.i(TAG,"Post: " +post.getDescription());
+                }
+            }
+        });
     }
 }
