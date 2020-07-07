c414e4e586f304c641979becc6c8a1dede006542
Thu's <thuvo@fb.com>
2020-07-07 13:01:00 -0400

    log out feature

--- app/src/main/java/com/fbu/instagrom/MainActivity.java
+++ app/src/main/java/com/fbu/instagrom/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -13,7 +16,9 @@ import android.widget.Toast;
 import com.fbu.instagrom.databinding.ActivityMainBinding;
 import com.fbu.instagrom.fragments.ComposeFragment;
 import com.fbu.instagrom.fragments.PostsFragment;
+import com.fbu.instagrom.fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
@@ -38,8 +43,8 @@ public class MainActivity extends AppCompatActivity {
                         fragment = new ComposeFragment();
                         break;
                     case R.id.action_profile:
-                        //TODO: update fragment
-                        fragment = new ComposeFragment();
+                        fragment = new ProfileFragment();
+                        break;
                     default:
                         fragment = new PostsFragment();
                         break;
@@ -50,6 +55,22 @@ public class MainActivity extends AppCompatActivity {
         });
         // Set default selection
         binding.bottomNavigation.setSelectedItemId(R.id.action_home);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_logout, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        ParseUser.logOut();
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        Log.i(TAG, currentUser==null? "Log out success" : "Log out fail");
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
+        return super.onOptionsItemSelected(item);
     }
 
 
